9368e2d059e0db87d0334f1fbd75c1daa8813078
Kunal Khatri <kunalkhatri137@gmail.com>
2022-10-19 15:46:46 +0530

    started nextjs, added moralis

---
+++ nextjs-smartcontract-lottery-fcc/pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head"
+import Image from "next/image"
+import styles from "../styles/Home.module.css"
+import Header from "../components/Header"
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Smart Contract Lottery</title>
+                <meta name="description" content="Smart Contract Lottery" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            {/* Header conncet button navbar */}
+            <Header />
+            Hi
+        </div>
+    )
+}
